b74064349a865ebd69a23ce3ea0922ac753aa596
sb5617 <sb5617@skbl-422-005.ph.ic.ac.uk>
2020-02-13 16:33:20 +0000

    Cant get initialisation to work

--- Main.asm
+++ Main.asm
@@ -1,6 +1,6 @@
 #include p18f87k22.inc
 
-	extern  glcd_start, Keypad_test
+	extern  glcd_start_left, glcd_start_right, Keypad_test, clear_glcd, glcd_grid
 
 Main code
 ;goto Shiqi
@@ -9,8 +9,16 @@ Shiqi
         call Keypad_test
 	goto Shiqi
 	return
-	
-goto glcd_start
+
+;Clear GLCD
+call glcd_start_left
+call clear_glcd
+call glcd_start_right
+call clear_glcd
+
+;Give the glcd the initial grid and player turns
+;call glcd_grid
+
 
 
 	
