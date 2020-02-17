1f37a82f6c3a95f1e95c0e411524f2180fb7df83
sb5617 <sb5617@skbl-422-005.ph.ic.ac.uk>
2020-02-17 14:45:27 +0000

    Good Stuff

--- Keypad.asm
+++ Keypad.asm
@@ -1,8 +1,7 @@
 #include p18f87k22.inc
 	
 	global  Keypad_test
-	extern	LCD_delay_x4us
-	
+	extern	LCD_delay_x4us	
 	;goto	start1
 	
 	;org 0x100		    ; Main code starts here at address 0x100
@@ -41,40 +40,40 @@ Keypad_test
 	
 Lemon1
 	movlw	0x0E
-	CPFSEQ	PORTE	       ;equal to first row 
+	CPFSEQ	PORTE	       ;equal to first column (1,4,7,A) 
 	goto	Lemon2
 	movlw	0xF0	       ;Keypad column
 	movwf	TRISE, ACCESS
 	call	LCD_delay_x4us
 	movlw	0x70
-	CPFSEQ	PORTE	       ;equal to first column 
+	CPFSEQ	PORTE	       ;equal to fourth row
 	goto	Ade1
-	;1 Pressed
-	movlw	0x01	
+	;A Pressed
+	movlw	0x0A	
 	movwf	PORTJ, ACCESS
 	goto	Finish
 Ade1	
 	movlw	0xB0
-	CPFSEQ	PORTE	       ;equal to second column 
+	CPFSEQ	PORTE	       ;equal to third row
 	goto	Ade2
-	;4 Pressed
-	movlw	0x04	
-	movwf	PORTJ, ACCESS
-	goto	Finish
-Ade2
-	movlw	0xD0
-	CPFSEQ	PORTE	       ;equal to third column 
-	goto	Ade3
 	;7 Pressed
 	movlw	0x07	
 	movwf	PORTJ, ACCESS
 	goto	Finish
+Ade2
+	movlw	0xD0
+	CPFSEQ	PORTE	       ;equal to second row
+	goto	Ade3
+	;4 Pressed
+	movlw	0x04	
+	movwf	PORTJ, ACCESS
+	goto	Finish
 Ade3
 	movlw	0xE0
-	CPFSEQ	PORTE	       ;equal to fourth column 
+	CPFSEQ	PORTE	       ;equal to first row
 	goto	Lemon5
-	;A Pressed
-	movlw	0x0A	
+	;1 Pressed
+	movlw	0x01	
 	movwf	PORTJ, ACCESS
 	goto	Finish
 
@@ -82,40 +81,40 @@ Ade3
 	
 Lemon2
 	movlw	0x0D
-	CPFSEQ	PORTE	       ;equal to second row 
+	CPFSEQ	PORTE	       ;equal to second column (2,5,8,0)
 	goto	Lemon3
 	movlw	0xF0	       ;Keypad column
 	movwf	TRISE, ACCESS
 	call	LCD_delay_x4us
 	movlw	0x70
-	CPFSEQ	PORTE	       ;equal to first column
+	CPFSEQ	PORTE	       ;equal to fourth row
 	goto	Ade4
-	;2 Pressed
-	movlw	0x02	
+	;0 Pressed
+	movlw	0x00	
 	movwf	PORTJ, ACCESS
 	goto	Finish
 Ade4
 	movlw	0xB0
-	CPFSEQ	PORTE	       ;equal to second column 
+	CPFSEQ	PORTE	       ;equal to third row
 	goto	Ade5
-	;5 Pressed
-	movlw	0x05	
-	movwf	PORTJ, ACCESS
-	goto	Finish
-Ade5
-	movlw	0xD0
-	CPFSEQ	PORTE	       ;equal to third column 
-	goto	Ade6
 	;8 Pressed
 	movlw	0x08	
 	movwf	PORTJ, ACCESS
 	goto	Finish
+Ade5
+	movlw	0xD0
+	CPFSEQ	PORTE	       ;equal to second row
+	goto	Ade6
+	;5 Pressed
+	movlw	0x05	
+	movwf	PORTJ, ACCESS
+	goto	Finish
 Ade6
 	movlw	0xE0
-	CPFSEQ	PORTE	       ;equal to fourth column 
+	CPFSEQ	PORTE	       ;equal to first row
 	goto	Lemon5
-	;0 Pressed
-	movlw	0x00	
+	;2 Pressed
+	movlw	0x02	
 	movwf	PORTJ, ACCESS
 	goto	Finish
 	
@@ -124,39 +123,39 @@ Ade6
 
 Lemon3	
 	movlw	0x0B
-	CPFSEQ	PORTE	       ;equal to third row
+	CPFSEQ	PORTE	       ;equal to third column (3,6,9,B)
 	goto	Lemon4
 	movlw	0xF0	       ;Keypad column
 	movwf	TRISE, ACCESS
 	call	LCD_delay_x4us
-	CPFSEQ	PORTE, 0x70    ;equal to first column 
+	CPFSEQ	PORTE, 0x70    ;equal to fourth row
 	goto	Ade7
-	;3 Pressed
-	movlw	0x03	
+	;B Pressed
+	movlw	0x0B	
 	movwf	PORTJ, ACCESS
 	goto	Finish
 Ade7
 	movlw	0xB0
-	CPFSEQ	PORTE	       ;equal to second column 
+	CPFSEQ	PORTE	       ;equal to third row
 	goto	Ade8
-	;6 Pressed
-	movlw	0x06	
-	movwf	PORTJ, ACCESS
-	goto	Finish
-Ade8
-	movlw	0xD0
-	CPFSEQ	PORTE	       ;equal to third column 
-	goto	Ade9
 	;9 Pressed
 	movlw	0x09	
 	movwf	PORTJ, ACCESS
 	goto	Finish
+Ade8
+	movlw	0xD0
+	CPFSEQ	PORTE	       ;equal to second row
+	goto	Ade9
+	;6 Pressed
+	movlw	0x06	
+	movwf	PORTJ, ACCESS
+	goto	Finish
 Ade9
 	movlw	0xE0
-	CPFSEQ	PORTE	       ;equal to fourth column 
+	CPFSEQ	PORTE	       ;equal to first row
 	goto	Lemon5
-	;B Pressed
-	movlw	0x0B	
+	;3 Pressed
+	movlw	0x03	
 	movwf	PORTJ, ACCESS
 	goto	Finish
 	
@@ -165,40 +164,40 @@ Ade9
 	
 Lemon4	
 	movlw	0x07
-	CPFSEQ	PORTE	       ;equal to fourth row
+	CPFSEQ	PORTE	       ;equal to fourth column (F,E,D,C)
 	goto	Lemon5
 	movlw	0xF0	       ;Keypad column
 	movwf	TRISE, ACCESS
 	call	LCD_delay_x4us
 	movlw	0x70
-	CPFSEQ	PORTE	       ;equal to first column 
+	CPFSEQ	PORTE	       ;equal to fourth row 
 	goto	Ade10
-	;F Pressed
-	movlw	0xFF	
+	;C Pressed
+	movlw	0x0C	
 	movwf	PORTJ, ACCESS
 	goto	Finish
 Ade10
 	movlw	0xB0
-	CPFSEQ	PORTE	       ;equal to second column 
+	CPFSEQ	PORTE	       ;equal to third row 
 	goto	Ade11
-	;E Pressed
-	movlw	0x0E	
-	movwf	PORTJ, ACCESS
-	goto	Finish
-Ade11
-	movlw	0xD0
-	CPFSEQ	PORTE	       ;equal to third column 
-	goto	Ade12
 	;D Pressed
 	movlw	0x0D	
 	movwf	PORTJ, ACCESS
 	goto	Finish
+Ade11
+	movlw	0xD0
+	CPFSEQ	PORTE	       ;equal to second row 
+	goto	Ade12
+	;E Pressed
+	movlw	0x0E	
+	movwf	PORTJ, ACCESS
+	goto	Finish
 Ade12
 	movlw	0xE0
-	CPFSEQ	PORTE	       ;equal to fourth column 
+	CPFSEQ	PORTE	       ;equal to first column 
 	goto	Lemon5
-	;C Pressed
-	movlw	0x0C	
+	;F Pressed
+	movlw	0x0F	
 	movwf	PORTJ, ACCESS
 	goto	Finish
 	
@@ -216,4 +215,5 @@ Finish
 	return
 
 	
-end
+
+    end
